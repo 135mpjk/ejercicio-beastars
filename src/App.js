1fe9bd031e3cddbacc836509c8d00981fb9c2b74
Paula_Gil <135mariapaula@gmail.com>
2021-06-11 11:56:36 -0500

    rutas y botones

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './utils/images/logo.svg'
 import './App.css';
-import Todo from './components/Todo'
-import Cuerpo from './components/Cuerpo'
-import Footer from './components/Footer'
-import Title from './components/Title'
 import GlobalStyle from './globalStyles';
-
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Contact from "./pages/Contact/Contact"
+import Rouis from "./pages/Rouis/Rouis"
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle/>
-      <Title/>
-      <Todo/>
-      <Cuerpo/>
-      <Footer/>
+     <Router>
+     <GlobalStyle/>
+     <Switch>
+       <Route path="/" exact component={Home}/>
+       <Route path="/contact" exact component={Contact}/>
+       <Route path="/rouis" exact component={Rouis}/>
+     </Switch>
+     </Router>
     </div>
   );
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import '../utils/css/Title.estilos.css'
+import {Link} from "react-router-dom"
+
 function Title(){
     return(
         <div className="Container_Title">
             <h1 id="principalTitle">BEASTARS</h1>
-
+            <Link to="/contact">
+                <button>LEGOSHI</button>
+            </Link>
+            <Link to="/">
+                <button>HARU</button>
+            </Link>
+            <Link to="/rouis">
+                <button>ROUIS</button>
+            </Link>
         </div>
     )
 }
